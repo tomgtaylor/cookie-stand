3afd29322e5b1d01e4695321ac3853b3a5cd03b9
Tom Taylor <tomgtaylor@yahoo.com>
2022-06-14 11:15:42 -0700

    updated class06-objects lab

--- app.js
+++ app.js
@@ -1,12 +1,12 @@
- 'use strict'
+'use strict'
 
+let hours = ['6am', '7am', '8am', '9am', '10am', '11am', '12pm', '1pm', '2pm', '3pm', '4pm', '5pm', '6am', '7am', '8am'];
 
-// //random number of customers per hour
-// function generateRandom = (min, max, avg) {
-//     return Math.round(Math.random() * (max-min) + min);
-// // }
+function generateNumberBetween(min, max){
+    return Math.round(Math.random() * (max-min) + min);
+}
 
-// // console.log(generateRandom)
+//-----------------------------------------------------
 
 let seattle = { 
     shopName: 'Seattle',
@@ -16,7 +16,7 @@ let seattle = {
     hrSales: [],
     totalSales: 0,
 // }
-
+//-----------------------------------------------------
 let Tokyo = {
     shopName: 'Tokyo',
     minCust: 3,
@@ -25,7 +25,7 @@ let Tokyo = {
     hrSales: [],
     totalSales: 0,
 }
-
+//-----------------------------------------------------
 let Dubai = {
     shopName: 'Dubai',
     minCust: 11,
@@ -34,7 +34,7 @@ let Dubai = {
     hrSales: [],
     totalSales: 0,
 }
-
+//-----------------------------------------------------
 let Paris = {
     shopName: 'Paris',
     minCust: 20,
@@ -43,70 +43,52 @@ let Paris = {
     hrSales: [],
     totalSales: 0,
 }
-
+//-----------------------------------------------------
 let Lima = {
     shopName: 'Lima',
     minCust: 2,
     maxCust: 16,
     avgSales: 4.6,
-    hrSales: [],
+    custAtEachHour: [],
     totalSales: 0,
-}
+    //-----------------------------------------------------
+      genCustPerHour: function (){                          //Generates Random number of customers per hour
+    for (let i = 0; i < hours.length; i++) {
+        console.log(number);
+        this.custAtEachHour.push(number);
+      }       
 
-let hours = ['6am', '7am', '8am', '9am', '10am', '11am', '12pm', '1pm', '2pm', '3pm', '4pm', '5pm', '6am', '7am', '8am'];
+      return this.custAtEachHour;
+    }
+    //-----------------------------------------------------
+    genCookiesPerHour: function (custAtEachHour) {          //Generates Random number of cookies per hour
+
+        let result = []
+        for (let i = 0; i< custAtEachHour.length; i++) {
 
 
-
-// // function avgCustomerPerHour =(min, max, avg) {
-// // 	let hourlyArray = [];
-// // 	for (i = 0, i <= hours.length, i++) {	
-// //     }
-// // }
-// // return Math.round (Math.random() * (max-min)) + min (* avg);
-
-// // function 
-
-// function randomNumber = ()
-
-// seattle.
-
-// function customersPerHour(min, max, avg){
-//     let hrArray = [];
-//     for (i = 0; i <= 13; i++) {
-//         let hrArray.push(Math.round(Math.random() * (max - min) + min) * avg);
-//     }
-//     return hrArray;
-// }
-
-// function shop(name, min, max, avg) {
-//     shop.name = name;
-//     shop.minimum = min;
-//     shop.maximum = max;
-//     shop.avgSales = avg;
-//     shop.hrArray = customersPerHour(min, max, avg); 
-// }
-
-// let seattle = new shop('Seattle', 23, 65, 6.3);
-
-//-----------------------------------------
-//-----------------------------------------
-//-----------------------------------------
-
-// const seattle = {
-//     location: 'Seattle',
-//     openHr: 6,
-//     closeHr: 20,  //military time
-//     minCustomers: 23,
-//     maxCustomers: 65,
-//     avgCookieSales: 6.3,
-//         calcSalesByHour(){
-//             let hour = [];
-//             let hrSales: [];
-//             for (let i = seattle.openHr; i <= seattle.closeHr; i++){
-//                 let randomCustomer = (Math.round(Math.random() * (seattle.maxCustomers - seattle.minCustomers) + seattle.minCustomers)); 
-//                 if (i < 13) {
-//                     hour = (i "pm")
-                }
-            }
+        custAtEachHour[1] * this.avgCookiePerCustomer;
         }
+
+        return []
+    }
+    //-----------------------------------------------------
 }
+//-----------------------------------------------------
+
+seattle.genCustPerHour();
+seatte.genCookiesPerHour(seattle.genCustPerHour);
+console.log(seattle);
+
+funtion displayHours(sales) {
+    let seattleResults = document.getElementById('seattle-results');
+
+    let listElement = document.createElement('ul');
+    for (let i = 0; i < sales.length; i++) {
+        let listItemElement = document.createElement('li');
+        listItemElement.textContent = sales;
+        listElement.appendChild(listItemElement);
+    }
+}
+
+displayHours(seattleCookiesPerHour);

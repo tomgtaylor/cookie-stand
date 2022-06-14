7adfb63f6d9c59e826b687186d4f9b3f5feede39
Tom Taylor <tomgtaylor@yahoo.com>
2022-06-13 18:14:26 -0700

    added app.js, index.html, sales.html

---
+++ app.js
@@ -0,0 +1,47 @@
+'use strict'
+
+// function generateRandom(min, max) {
+//        return Math.round(Math.random() * (max-min)) + min; 
+// }
+
+// function avgCustomerPerHour(min, max){
+//     return Math.round(Math.random() * (max-min)) + min;
+// } 
+
+
+// let Seattle = {
+//     name: 'Seattle' [0],
+//     min:23,
+//     max:65 ,
+//     hrsOpen: 14,
+//     avgCookies: 6.3,
+    
+    
+// }
+
+// console.log(avgCustomerPerHour(23,65));
+// let sectionEl = document.write();
+//-----------------------------------------
+// below will create 
+function avgCustomersPerHour(min, max, avg) {
+    let perHourArray = []
+    for (x = 0; x >= 13; x++) {
+       return Math.round(math.random(perHourArray()));
+    }
+} 
+//console.log(avgCustomersPerHour(23, 65, 6.3));
+
+let seattle = { 
+    shopName: 'Seattle',
+    minCust: 23,
+    maxCust: 65,
+    avgCookieSales: 6.3,
+    hourlyTotal: 
+    
+}
+
+//-----------------------------------------
+//-----------------------------------------
+//-----------------------------------------
+
+

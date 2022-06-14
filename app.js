6765378dd7229f1169f82e7982c797938c774a54
Tom Taylor <tomgtaylor@yahoo.com>
2022-06-13 23:36:23 -0700

    js changes

--- app.js
+++ app.js
@@ -1,47 +1,112 @@
-'use strict'
-
-// function generateRandom(min, max) {
-//        return Math.round(Math.random() * (max-min)) + min; 
-// }
-
-// function avgCustomerPerHour(min, max){
-//     return Math.round(Math.random() * (max-min)) + min;
-// } 
+ 'use strict'
 
 
-// let Seattle = {
-//     name: 'Seattle' [0],
-//     min:23,
-//     max:65 ,
-//     hrsOpen: 14,
-//     avgCookies: 6.3,
-    
-    
-// }
+// //random number of customers per hour
+// function generateRandom = (min, max, avg) {
+//     return Math.round(Math.random() * (max-min) + min);
+// // }
 
-// console.log(avgCustomerPerHour(23,65));
-// let sectionEl = document.write();
-//-----------------------------------------
-// below will create 
-function avgCustomersPerHour(min, max, avg) {
-    let perHourArray = []
-    for (x = 0; x >= 13; x++) {
-       return Math.round(math.random(perHourArray()));
-    }
-} 
-//console.log(avgCustomersPerHour(23, 65, 6.3));
+// // console.log(generateRandom)
 
 let seattle = { 
     shopName: 'Seattle',
     minCust: 23,
     maxCust: 65,
-    avgCookieSales: 6.3,
-    hourlyTotal: 
-    
+    avgSales: 6.3,
+    hrSales: [],
+    totalSales: 0,
+// }
+
+let Tokyo = {
+    shopName: 'Tokyo',
+    minCust: 3,
+    maxCust: 24,
+    avgSales: 1.2,
+    hrSales: [],
+    totalSales: 0,
 }
 
+let Dubai = {
+    shopName: 'Dubai',
+    minCust: 11,
+    maxCust: 38,
+    avgSales: 3.7,
+    hrSales: [],
+    totalSales: 0,
+}
+
+let Paris = {
+    shopName: 'Paris',
+    minCust: 20,
+    maxCust: 38,
+    avgSales: 2.3,
+    hrSales: [],
+    totalSales: 0,
+}
+
+let Lima = {
+    shopName: 'Lima',
+    minCust: 2,
+    maxCust: 16,
+    avgSales: 4.6,
+    hrSales: [],
+    totalSales: 0,
+}
+
+let hours = ['6am', '7am', '8am', '9am', '10am', '11am', '12pm', '1pm', '2pm', '3pm', '4pm', '5pm', '6am', '7am', '8am'];
+
+
+
+// // function avgCustomerPerHour =(min, max, avg) {
+// // 	let hourlyArray = [];
+// // 	for (i = 0, i <= hours.length, i++) {	
+// //     }
+// // }
+// // return Math.round (Math.random() * (max-min)) + min (* avg);
+
+// // function 
+
+// function randomNumber = ()
+
+// seattle.
+
+// function customersPerHour(min, max, avg){
+//     let hrArray = [];
+//     for (i = 0; i <= 13; i++) {
+//         let hrArray.push(Math.round(Math.random() * (max - min) + min) * avg);
+//     }
+//     return hrArray;
+// }
+
+// function shop(name, min, max, avg) {
+//     shop.name = name;
+//     shop.minimum = min;
+//     shop.maximum = max;
+//     shop.avgSales = avg;
+//     shop.hrArray = customersPerHour(min, max, avg); 
+// }
+
+// let seattle = new shop('Seattle', 23, 65, 6.3);
+
 //-----------------------------------------
 //-----------------------------------------
 //-----------------------------------------
 
-
+// const seattle = {
+//     location: 'Seattle',
+//     openHr: 6,
+//     closeHr: 20,  //military time
+//     minCustomers: 23,
+//     maxCustomers: 65,
+//     avgCookieSales: 6.3,
+//         calcSalesByHour(){
+//             let hour = [];
+//             let hrSales: [];
+//             for (let i = seattle.openHr; i <= seattle.closeHr; i++){
+//                 let randomCustomer = (Math.round(Math.random() * (seattle.maxCustomers - seattle.minCustomers) + seattle.minCustomers)); 
+//                 if (i < 13) {
+//                     hour = (i "pm")
+                }
+            }
+        }
+}

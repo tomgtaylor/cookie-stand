8db2cbf93c9a0115a7df51c818102ddf6c781434
Tom Taylor <tomgtaylor@yahoo.com>
2022-06-14 23:09:19 -0700

    updated, but confused and tired

--- app.js
+++ app.js
@@ -1,9 +1,9 @@
 'use strict'
 
-let hours = ['6am', '7am', '8am', '9am', '10am', '11am', '12pm', '1pm', '2pm', '3pm', '4pm', '5pm', '6am', '7am', '8am'];
+let hours = ['6am', '7am', '8am', '9am', '10am', '11am', '12pm', '1pm', '2pm', '3pm', '4pm', '5pm', '6pm', '7pm', '8pm'];
 
 
-//-----------------------------------------------------
+//-------------------Object 1----------------------------------
 
 let seattle = { 
     shopName: 'Seattle',
@@ -12,7 +12,32 @@ let seattle = {
     avgSales: 6.3,
     hrSales: [],
     totalSales: 0,
-// }
+    hourlyCust: [],
+      //object method (function as a property of an object)
+    generateRandom: function(){
+      let randomNumber = Math.round(Math.random() * (this.maxCust - this.minCust) + this.minCust);
+      //console.log(randomNumber);
+      return randomNumber;
+    },
+    generateHourlyCustomers: function(){
+      for (let i = 0; i < hours.length; i++){
+        //console.log(hours[i]);
+        //this.generateRandom();
+        let randomNumber = this.generateRandom();
+        this.hourlyCust.push(randomNumber);
+      }
+      console.log(this.hourlyCust);
+    },
+    generateCookieSales: function(){
+      this.generateHourlyCustomers();
+      for (let i = 0; i < hours.length; i++){
+        this.hrSales.push(Math.ceil(this.hourlyCust[i] * this.avgSales));  //calc the  sales per hour and pushing it into the empty array in hrSales: []
+        //console.log(this.hourlyCust[i]);
+      }
+    },
+};
+
+
 //-----------------------------------------------------
 let tokyo = {
     shopName: 'Tokyo',
@@ -21,7 +46,30 @@ let tokyo = {
     avgSales: 1.2,
     hrSales: [],
     totalSales: 0,
-}
+    hourlyCust: [],
+      //object method (function as a property of an object)
+    generateRandom: function(){
+      let randomNumber = Math.round(Math.random() * (this.maxCust - this.minCust) + this.minCust);
+      //console.log(randomNumber);
+      return randomNumber;
+    },
+    generateHourlyCustomers: function(){
+      for (let i = 0; i < hours.length; i++){
+        //console.log(hours[i]);
+        //this.generateRandom();
+        let randomNumber = this.generateRandom();
+        this.hourlyCust.push(randomNumber);
+      }
+      console.log(this.hourlyCust);
+    },
+    generateCookieSales: function(){
+      this.generateHourlyCustomers();
+      for (let i = 0; i < hours.length; i++){
+        this.hrSales.push(Math.ceil(this.hourlyCust[i] * this.avgSales));  //calc the  sales per hour and pushing it into the empty array in hrSales: []
+        //console.log(this.hourlyCust[i]);
+      }
+    },
+};
 //-----------------------------------------------------
 let dubai = {
     shopName: 'Dubai',
@@ -30,7 +78,30 @@ let dubai = {
     avgSales: 3.7,
     hrSales: [],
     totalSales: 0,
-}
+    hourlyCust: [],
+      //object method (function as a property of an object)
+    generateRandom: function(){
+      let randomNumber = Math.round(Math.random() * (this.maxCust - this.minCust) + this.minCust);
+      //console.log(randomNumber);
+      return randomNumber;
+    },
+    generateHourlyCustomers: function(){
+      for (let i = 0; i < hours.length; i++){
+        //console.log(hours[i]);
+        //this.generateRandom();
+        let randomNumber = this.generateRandom();
+        this.hourlyCust.push(randomNumber);
+      }
+      console.log(this.hourlyCust);
+    },
+    generateCookieSales: function(){
+      this.generateHourlyCustomers();
+      for (let i = 0; i < hours.length; i++){
+        this.hrSales.push(Math.ceil(this.hourlyCust[i] * this.avgSales));  //calc the  sales per hour and pushing it into the empty array in hrSales: []
+        //console.log(this.hourlyCust[i]);
+      }
+    },
+};
 //-----------------------------------------------------
 let paris = {
     shopName: 'Paris',
@@ -39,7 +110,30 @@ let paris = {
     avgSales: 2.3,
     hrSales: [],
     totalSales: 0,
-}
+    hourlyCust: [],
+      //object method (function as a property of an object)
+    generateRandom: function(){
+      let randomNumber = Math.round(Math.random() * (this.maxCust - this.minCust) + this.minCust);
+      //console.log(randomNumber);
+      return randomNumber;
+    },
+    generateHourlyCustomers: function(){
+      for (let i = 0; i < hours.length; i++){
+        //console.log(hours[i]);
+        //this.generateRandom();
+        let randomNumber = this.generateRandom();
+        this.hourlyCust.push(randomNumber);
+      }
+      console.log(this.hourlyCust);
+    },
+    generateCookieSales: function(){
+      this.generateHourlyCustomers();
+      for (let i = 0; i < hours.length; i++){
+        this.hrSales.push(Math.ceil(this.hourlyCust[i] * this.avgSales));  //calc the  sales per hour and pushing it into the empty array in hrSales: []
+        //console.log(this.hourlyCust[i]);
+      }
+    },
+};
 
 //-----------------------------------------------------
 
@@ -48,119 +142,70 @@ let lima = {
     minCust: 2,
     maxCust: 16,
     avgSales: 4.6,
-    custHourSales: [],
+    hrSales: [],
     totalSales: 0,
-}
+    hourlyCust: [],
+      //object method (function as a property of an object)
+    generateRandom: function(){
+      let randomNumber = Math.round(Math.random() * (this.maxCust - this.minCust) + this.minCust);
+      //console.log(randomNumber);
+      return randomNumber;
+    },
+    generateHourlyCustomers: function(){
+      for (let i = 0; i < hours.length; i++){
+        //console.log(hours[i]);
+        //this.generateRandom();
+        let randomNumber = this.generateRandom();
+        this.hourlyCust.push(randomNumber);
+      }
+      console.log(this.hourlyCust);
+    },
+    generateCookieSales: function(){
+      this.generateHourlyCustomers();
+      for (let i = 0; i < hours.length; i++){
+        this.hrSales.push(Math.ceil(this.hourlyCust[i] * this.avgSales));  //calc the  sales per hour and pushing it into the empty array in hrSales: []
+        //console.log(this.hourlyCust[i]);
+      }
+      function list(shop) {
+        let ul = document.getElementById(shopName);
+        for (let i = 0; i < hours.length; i++) {
+          let li = document.createElement('li');
+          li.appendChild(document.createTextNode(Hours[i] + ': ' + Math.floor(shop.hourlySales[i] * city.avgSales) + ' cookies'));
+          ul.appendChild(li);
+        }
+        let li = document.createElement('li');
+        li.appendChild(document.createTextNode('Total: ' + shopName.totalSales + ' cookies'));
+        ul.appendChild(li);
+      }
+      return list(lima);
+    },
+  };
 
-//-----------------Function 1: Generate a Random Number------------------------------------
-
-function generateRandom(min, max) {
-    return Math.round(Math.random() * (max - min) + min);
-  }
-
-//-------------------Function 2: Calculate the Total Hourly Sales for any specified location-------------------
-
-function calculateTotalSales(shop) {
-    let totalSales = 0;
-    for (let i = 0; i < shop.custHourSales.length; i++) {
-        totalSales = Math.round(shop.custHourSales[i] * shop.avgSales);   /// multiply b avg gives us an average, according to David Souther.
-    }
-    return shop.totalSales = totalSales;
-  }
-
-//-----------------------------------------------------------------------
-
-function hourlySalesInDay () {
-    for (let i = 0; i < hours.length; i++) {         
-        
-        //"seattle" specifies the above object; "custHourSales[]" specifies the property in the object;
-        // [i] fills it in with the above "calculateTotalSales()"" function, 
-
-      seattle.custHourSales[i] = generateRandom(seattle.minCust, seattle.maxCust); 
-      calculateTotalSales(seattle);
-
-      tokyo.custHourSales[i] = generateRandom(tokyo.minCust, tokyo.maxCust);
-      calculateTotalSales(tokyo);
-
-      dubai.custHourSales[i] = generateRandom(dubai.minCust, dubai.maxCust);
-      calculateTotalSales(dubai);
-
-      paris.custHourSales[i] = generateRandom(paris.minCust, paris.maxCust);
-      calculateTotalSales(paris);
-
-      lima.custHourSales[i] = generateRandom(lima.minCust, lima.maxCust);
-      calculateTotalSales(lima);
-    }
-  }
-
-//-----------------------------------------------------------------------
-
-  function showDataLists(shop) {
-
-    let ul = document.getElementById(shop.shopName);
-    for (let i = 0; i < Hours.length; i++) {
-    let li = document.createElement('li');   //id to connect JS with HTML
-    li.appendChild(document.createTextNode('At ${hours[i]}, the total will be ${Math.custHourSales[i]} * shop.avgSales) cookies.');
-    ul.appendChild(li);   
-
-    }
-    let li = document.createElement('li');
-    li.appendChild(document.createTextNode('Total: ' + shop.total + ' cookies')); 
-    ul.appendChild(li);     //append will fill in a <li> at the end of the ul/ol?
-  }
-
-hourlySalesInDay();
-showDataLists(seattle);
-showDataLists(tokyo);
-showDataLists(dubai);
-showDataLists(paris);
-showDataLists(lima);
-
-    //-----------------------------------------------------
-
-    ('Total: ' + shop.total + ' cookies')
-
-    (`Total: ${shop.total} cookies.`)
-
-    //-----------------------------------------------------
-
-//       genCustPerHour: function (){                          //Generates Random number of customers per hour
-//     for (let i = 0; i < hours.length; i++) {
-//         console.log(number);
-//         this.custAtEachHour.push(number);
-//       }       
-
-//       return this.custAtEachHour;
-//     }
-//     //-----------------------------------------------------
-//     genCookiesPerHour: function (custAtEachHour) {          //Generates Random number of cookies per hour
-
-//         let result = []
-//         for (let i = 0; i< custAtEachHour.length; i++) {
-
-
-//         custAtEachHour[1] * this.avgCookiePerCustomer;
-//         }
-
-//         return []
-//     }
-//     //-----------------------------------------------------
-// }
-// //-----------------------------------------------------
-
-// seattle.genCustPerHour();
-// seatte.genCookiesPerHour(seattle.genCustPerHour);
-// console.log(seattle);
-
-// funtion displayHours(sales) {
-//     let seattleResults = document.getElementById('seattle-results');
-
-//     let listElement = document.createElement('ul');
-//     for (let i = 0; i < sales.length; i++) {
-//         let listItemElement = document.createElement('li');
-//         listItemElement.textContent = sales;
-//         listElement.appendChild(listItemElement);
-//     }
+// function list(shop) {
+//   let ul = document.getElementById(shop.shopName);
+//   for (let i = 0; i < hours.length; i++) {
+//     let li = document.createElement('li');
+//     li.appendChild(document.createTextNode(dayHours[i] + ': ' + Math.floor(shop.hourlySales[i] * city.avgSales) + ' cookies'));
+//     ul.appendChild(li);
+//   }
+//   let li = document.createElement('li');
+//   li.appendChild(document.createTextNode('Total: ' + city.total + ' cookies'));
+//   ul.appendChild(li);
 // }
 
-// displayHours(seattleCookiesPerHour);
+// list(seattle);
+
+
+
+// list(tokyo);
+// list(dubai);
+// list(paris);
+// list(lima);
+
+// let stores = 
+// function render (stores) {
+//   let tableEl = document.getElementById('stores');
+//   let tableRowEl = document.createElement('tr');
+//   let nameEl = document.createElement('td');
+//   let 
+// }
